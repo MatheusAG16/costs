4843a3e9a0ea9ce12a12783d9daf45003b4df164
MatheusAG16 <mateusandradegermano@gmail.com>
2025-04-13 15:03:41 -0300

    Update

--- src/components/pages/Company.js
+++ src/components/pages/Company.js
@@ -1,5 +1,21 @@
 function Company() {
-    return <h1>Company</h1>
+  return (
+    <>
+      <h1>Sobre</h1>
+      <p>
+        A Costs é uma plataforma especializada no gerenciamento eficiente e
+        simplificado de projetos que demandam recursos dentro de sua empresa.
+        Com ela, é possível criar e organizar diversos projetos, além de alocar
+        recursos específicos para áreas estratégicas, como Desenvolvimento,
+        Design, Infraestrutura e Planejamento.
+        <br />
+        <br />
+        Essa abordagem facilita o processo de gestão de projetos, proporcionando
+        mais controle, clareza e eficiência no uso dos recursos disponíveis,
+        promovendo melhores resultados para o seu negócio!
+      </p>
+    </>
+  );
 }
 
-export default Company
+export default Company;
